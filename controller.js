a8e8c80c7b09ca604eb3c3b2accf11750acef07b
thaleprechaun <oakinbiyiusman@gmail.com>
2021-09-01 23:07:32 +0100

    Add details for tracking user msisdn from ayoba

--- controller.js
+++ controller.js
@@ -1,12 +1,28 @@
+const {format} = require('date-fns');
 var axios = require('axios');
-let token = 'NJ06A666-741F-484E-AEC9-8B91D2CP'
+let token = 'NJ06A666-741F-484E-AEC9-8B91D2CP';
 async function subscribe(msisdn, serviceid, channel) {
   try {
       const info = { msisdn, serviceid, channel };
+      
+      // Details to track msisdn coming from Ayoba
+      const date = format(new Date(), 'yyyyMMdd');
+      const trackChannel ="AyobaM";
+      const trackServiceTen = "LN10";
+      const trackServiceFifty = "LN50";
+      const tracktrxid = `${date}-ayoba-${Math.floor(Math.random() * 10000)}`;
+      const trackInfo = {msisdn, channel: trackChannel, service: info.serviceid == 526 ? trackServiceTen : trackServiceFifty, trxid: tracktrxid};
+      //Stops here
+      
+      const trackOptions = {
+        headers: { 'Content-Type': 'application/json'}
+      };
       const options = {
-          headers: { 'X-TOKEN': token, 'CPID': '11' }
+        headers: { 'X-TOKEN': token, 'CPID': '11' }
       };
       const response = await axios.post('http://89.107.63.145:8088/subscribe', info, options);
+      await axios.post('https://promo.ydafrica.com/sync/tracking/microayoba_req', trackInfo,  trackOptions);
+
       if(response.data.status === false){
         return { status: 400, msg: response.data.message };
       }
