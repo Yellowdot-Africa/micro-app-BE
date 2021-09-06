ed93ccc6e3de6381feff0a946a4586b527b12803
thaleprechaun <oakinbiyiusman@gmail.com>
2021-09-06 19:04:47 +0100

    Add filter to remove non-MTN numbers

--- controller.js
+++ controller.js
@@ -1,34 +1,41 @@
 const {format} = require('date-fns');
 var axios = require('axios');
 let token = 'NJ06A666-741F-484E-AEC9-8B91D2CP';
-async function subscribe(msisdn, serviceid, channel) {
-  try {
-      const info = { msisdn, serviceid, channel };
-      
-      // Details to track msisdn coming from Ayoba
-      const date = format(new Date(), 'yyyyMMdd');
-      const trackChannel ="AyobaM";
-      const trackServiceTen = "LN10";
-      const trackServiceFifty = "LN50";
-      const tracktrxid = `${date}-ayoba-${Math.floor(Math.random() * 10000)}`;
-      const trackInfo = {msisdn, channel: trackChannel, service: info.serviceid == 526 ? trackServiceTen : trackServiceFifty, trxid: tracktrxid};
-      const trackOptions = {
-        headers: { 'Content-Type': 'application/json'}
-      };
-      //Stops here
-      
-      const options = {
-        headers: { 'X-TOKEN': token, 'CPID': '11' }
-      };
-      const response = await axios.post('http://89.107.63.145:8088/subscribe', info, options);
-      await axios.post('https://promo.ydafrica.com/sync/tracking/microayoba_req', trackInfo,  trackOptions);
+const regexMTNNumber = /234((7025\d{6})|(7026\d{6})|(703\d{7})|(704\d{7})|(706\d{7})|(803\d{7})|(806\d{7})|(810\d{7})|(813\d{7})|(814\d{7})|(816\d{7})|(903\d{7})|(906\d{7}))/;
 
-      if(response.data.status === false){
+async function subscribe(msisdn, serviceid, channel) {
+  if(msisdn.match(regexMTNNumber)){
+    try {
+        const info = { msisdn, serviceid, channel };
+        // Details to track msisdn coming from Ayoba
+        const date = format(new Date(), 'yyyyMMdd');
+        const trackChannel ="AyobaM";
+        const trackServiceTen = "LN10";
+        const trackServiceFifty = "LN50";
+        const tracktrxid = `${date}-ayoba-${Math.floor(Math.random() * 10000)}`;
+        const trackInfo = {msisdn, channel: trackChannel, service: info.serviceid == 526 ? trackServiceTen : trackServiceFifty, trxid: tracktrxid};
+        const trackOptions = {
+          headers: { 'Content-Type': 'application/json'}
+        };
+        
+        const options = {
+          headers: { 'X-TOKEN': token, 'CPID': '11' }
+        };
+        const response = await axios.post('http://89.107.63.145:8088/subscribe', info, options);
+        
+        if(response.data.status === true){
+          await axios.post('https://promo.ydafrica.com/sync/tracking/microayoba_req', trackInfo,  trackOptions);
+          return {status: 200, data: response.data};
+        }
+
         return { status: 400, msg: response.data.message };
-      }
-      return {status: 200, data: response.data};
-  } catch (error) {
-    throw Error(error);
+
+    } catch (error) {
+      throw Error(error);
+    }
+  }
+  else{
+    return { status: 403, msg: 'Sorry, This service is restricted to MTN Nigeria Phone Numbers only' };
   }
 }
 
