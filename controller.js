3850151f0e96dd8047a530f4e56f9de5b261a4aa
thaleprechaun <oakinbiyiusman@gmail.com>
2022-07-08 09:21:21 +0100

    change aggregator subscription url and add details to env file

--- controller.js
+++ controller.js
@@ -1,39 +1,52 @@
+require('dotenv').config();
 const { format } = require('date-fns');
 var axios = require('axios');
 let token = 'NJ06A666-741F-484E-AEC9-8B91D2CP';
 const regexMTNNumber = /234((7025\d{6})|(7026\d{6})|(703\d{7})|(704\d{7})|(706\d{7})|(803\d{7})|(806\d{7})|(810\d{7})|(813\d{7})|(814\d{7})|(816\d{7})|(903\d{7})|(906\d{7}))/;
 
-async function subscribe(msisdn, serviceid, channel) {
+async function subscribe(pisipid, msisdn, channel, trxid) {
   if (msisdn.match(regexMTNNumber)) {
     try {
-      const info = { msisdn, serviceid, channel };
+      const info = { pisipid, msisdn, channel, trxid };
+      console.log(info);
       // Details to track msisdn coming from Ayoba
       const date = format(new Date(), 'yyyyMMdd');
       const trackChannel = "AyobaM";
       const trackServiceTen = "LN10";
       const trackServiceFifty = "LN50";
       const tracktrxid = `${date}-ayoba-${Math.floor(Math.random() * 10000)}`;
-      const trackInfo = { msisdn, channel: trackChannel, service: info.serviceid == 526 ? trackServiceTen : trackServiceFifty, trxid: tracktrxid };
+      const trackInfo = { msisdn, channel: trackChannel, service: info.pisipid == 526 ? trackServiceTen : trackServiceFifty, trxid: tracktrxid };
       const trackOptions = {
         headers: { 'Content-Type': 'application/json' }
       };
 
       const options = {
-        headers: { 'X-TOKEN': token, 'CPID': '11' }
+        headers: {
+          'Content-Type': 'application/json',
+          'vaspid': process.env.PISIMOBILE_VASPID,
+          'pisi-authorization-token': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InZhc3AiOiIxIn0sImlhdCI6MTY1NTg1MTczMSwiZXhwIjoxNjcxNDAzNzMxfQ.QgXtjAAt014JdRbtAN5Ow9KpLZjGmGO0NVvHxwDxN5U`
+      }
       };
-      const response = await axios.post('http://89.107.63.145:8088/subscribe', info, options);
-
-      if (response.data.status === true) {
-        await axios.post('https://promo.ydafrica.com/sync/tracking/microayoba_req', trackInfo, trackOptions);
-        return { status: 200, data: response.data };
+      const response = await axios.post(process.env.PISIMOBILE_SUBSCRIPTION_URL, info, options);
+      console.log(response.data);
+      if (response.data.success === true && response.data.message === 'Successful') {
+        let trackResponse = await axios.post('https://promo.ydafrica.com/sync/tracking/microayoba_req', trackInfo, trackOptions);
+        console.log(trackResponse.data, 'okay'); // This guy returns data: {status: 'success'}
+        let message = response.data;
+        return { message };
+      }
+      else if(response.data.success == false && response.data.message === 'You have Already Subscribed Requested Services'){
+        return { status: 400, msg: 'User is already subscribed to the service' };
       }
 
       else {
+        // ctx.throw(400, 'subscription request failed');
         return { status: 400, msg: response.data.message };
       }
 
     } catch (error) {
-      throw Error(error);
+      return error.response.data;
+      // throw Error(error);
     }
   }
   else {
--- route.js
+++ route.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const { format } = require('date-fns');
 const subscribe = require('./controller');
 const router = express.Router();
 
 router.post('/subscribe', async(req, res) => {
   try {
-      const { msisdn, serviceid, channel } = req.body;
-      const response = await subscribe(msisdn, serviceid, channel);
-      return res.status(200).json({ response });
+      let { pisipid, msisdn, channel, trxid } = req.body;
+      // console.log(pisipid, 'pisi');
+      // let serviceid;
+      // serviceid = pisipid == 526 ? '4' : pisipid == 322 ? '3' : '';
+      // console.log(serviceid, 'here');
+      //check that the service conforms with the svcId in the DB and PISIMOBILE side.
+      //3 - N50, 4 - N10, 5 - N20, 6 - N200 (3,4,5,6 are the ServiceIDs or pisipids of LN services on PISIMOBILE )
+      //N10 - 526, N20 - 525, N50 - 322, N200 - 527 (525, 322, 527 are the ServiceIDs or svcId in the DB)
+      const trxDate = format(new Date(), 'yyyyMMdd');
+      trxid = `ln${trxDate}${Math.floor(Math.random() * 10000)}`;
+
+      const response = await subscribe(pisipid, msisdn, channel, trxid);
+      return res.status(200).json(response);
   } catch (error) {
       return res.status(500).json({ error: error });
   }
