82ceb2c02f5205a703cacbd0ef8db9a319246b74
thaleprechaun <oakinbiyiusman@gmail.com>
2022-07-12 13:06:51 +0100

    handle error message from API

--- controller.js
+++ controller.js
@@ -5,52 +5,45 @@ let token = 'NJ06A666-741F-484E-AEC9-8B91D2CP';
 const regexMTNNumber = /234((7025\d{6})|(7026\d{6})|(703\d{7})|(704\d{7})|(706\d{7})|(803\d{7})|(806\d{7})|(810\d{7})|(813\d{7})|(814\d{7})|(816\d{7})|(903\d{7})|(906\d{7}))/;
 
 async function subscribe(pisipid, msisdn, channel, trxid) {
-  if (msisdn.match(regexMTNNumber)) {
-    try {
-      const info = { pisipid, msisdn, channel, trxid };
-      console.log(info);
-      // Details to track msisdn coming from Ayoba
-      const date = format(new Date(), 'yyyyMMdd');
-      const trackChannel = "AyobaM";
-      const trackServiceTen = "LN10";
-      const trackServiceFifty = "LN50";
-      const tracktrxid = `${date}-ayoba-${Math.floor(Math.random() * 10000)}`;
-      const trackInfo = { msisdn, channel: trackChannel, service: info.pisipid == 526 ? trackServiceTen : trackServiceFifty, trxid: tracktrxid };
-      const trackOptions = {
-        headers: { 'Content-Type': 'application/json' }
-      };
+  try {
+      if (msisdn.match(regexMTNNumber)) {
+        const info = { pisipid, msisdn, channel, trxid };
+        // Details to track msisdn coming from Ayoba
+        const date = format(new Date(), 'yyyyMMdd');
+        const trackChannel = "AyobaM";
+        const trackServiceTen = "LN10";
+        const trackServiceFifty = "LN50";
+        const tracktrxid = `${date}-ayoba-${Math.floor(Math.random() * 10000)}`;
+        const trackInfo = { msisdn, channel: trackChannel, service: info.pisipid == 526 ? trackServiceTen : trackServiceFifty, trxid: tracktrxid };
+        const trackOptions = {
+          headers: { 'Content-Type': 'application/json' }
+        };
 
-      const options = {
-        headers: {
-          'Content-Type': 'application/json',
-          'vaspid': process.env.PISIMOBILE_VASPID,
-          'pisi-authorization-token': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InZhc3AiOiIxIn0sImlhdCI6MTY1NTg1MTczMSwiZXhwIjoxNjcxNDAzNzMxfQ.QgXtjAAt014JdRbtAN5Ow9KpLZjGmGO0NVvHxwDxN5U`
+        const options = {
+          headers: {
+            'vaspid': process.env.PISIMOBILE_VASPID,
+            'pisi-authorization-token': process.env.PISIMOBILE_AUTH_TOKEN,
+            'Content-Type': 'application/json',
+        }
+        };
+        const response = await axios.post(process.env.PISIMOBILE_SUBSCRIPTION_URL, info, options);
+        if (response.data.success === true && response.data.message === 'Successful') {
+          let trackResponse = await axios.post('https://promo.ydafrica.com/sync/tracking/microayoba_req', trackInfo, trackOptions);
+          // trackResponse.data returns data: {status: 'success'}
+          return { status: 200, msg: 'Successful, kindly select "Accept Renewal" to qualify' };
+        }
+        if (response.data.success === false && response.data.message === 'You have Already Subscribed Requested Services') {
+          return { status: 400, msg: 'User is already subscribed to the service' };
+        }
+        else {
+          return { status: 400, msg: response.data.message };
+        }
       }
-      };
-      const response = await axios.post(process.env.PISIMOBILE_SUBSCRIPTION_URL, info, options);
-      console.log(response.data);
-      if (response.data.success === true && response.data.message === 'Successful') {
-        let trackResponse = await axios.post('https://promo.ydafrica.com/sync/tracking/microayoba_req', trackInfo, trackOptions);
-        console.log(trackResponse.data, 'okay'); // This guy returns data: {status: 'success'}
-        let message = response.data;
-        return { message };
-      }
-      else if(response.data.success == false && response.data.message === 'You have Already Subscribed Requested Services'){
-        return { status: 400, msg: 'User is already subscribed to the service' };
-      }
-
       else {
-        // ctx.throw(400, 'subscription request failed');
-        return { status: 400, msg: response.data.message };
+        return { status: 403, msg: 'Sorry, This service is restricted to MTN Nigeria Phone Numbers only' };
       }
-
-    } catch (error) {
-      return error.response.data;
-      // throw Error(error);
-    }
-  }
-  else {
-    return { status: 403, msg: 'Sorry, This service is restricted to MTN Nigeria Phone Numbers only' };
+  }catch (error) {
+    return {status: error.response.status, msg: error.response.data};
   }
 }
 
--- route.js
+++ route.js
@@ -5,21 +5,13 @@ const router = express.Router();
 
 router.post('/subscribe', async(req, res) => {
   try {
-      let { pisipid, msisdn, channel, trxid } = req.body;
-      // console.log(pisipid, 'pisi');
-      // let serviceid;
-      // serviceid = pisipid == 526 ? '4' : pisipid == 322 ? '3' : '';
-      // console.log(serviceid, 'here');
-      //check that the service conforms with the svcId in the DB and PISIMOBILE side.
-      //3 - N50, 4 - N10, 5 - N20, 6 - N200 (3,4,5,6 are the ServiceIDs or pisipids of LN services on PISIMOBILE )
-      //N10 - 526, N20 - 525, N50 - 322, N200 - 527 (525, 322, 527 are the ServiceIDs or svcId in the DB)
+      let { pisipid, msisdn, channel } = req.body;
       const trxDate = format(new Date(), 'yyyyMMdd');
       trxid = `ln${trxDate}${Math.floor(Math.random() * 10000)}`;
-
       const response = await subscribe(pisipid, msisdn, channel, trxid);
-      return res.status(200).json(response);
+      return res.status(response.status).json(response);
   } catch (error) {
-      return res.status(500).json({ error: error });
+      return res.status(500).json({ error: error.data });
   }
 });
 
